a1df595eafcb7891274a1ca40b73ac3107df4374
Yatin <ysagar@adobe.com>
2024-02-02 20:00:07 +0530

    chnges

--- blocks/coursecarousel/coursecarousel.js
+++ blocks/coursecarousel/coursecarousel.js
@@ -56,6 +56,9 @@ export default function decorate(block) {
     var navctr = '<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>';
     var newcont = '<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel"><div class="carousel-inner">'+markup+'</div>'+navctr+'</div>';
     parentEl.insertAdjacentHTML("afterbegin", newcont);
+    $('.carousel').carousel({
+      interval: 2000
+    });
     function generateMarkuploop() {
       const Mark = result.map((res,i) => generateMarkup(res,i)).join("");
       return Mark;
